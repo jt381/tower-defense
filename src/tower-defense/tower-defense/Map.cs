1293e5a2001be70cf05167b03052c2b98c1f5534
jontoomey <j.toomey@exeter.ac.uk>
2018-08-22 15:41:27 +0100

    Innitial commit

---
+++ src/tower-defense/tower-defense/Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tower_defense
+{
+    class Map
+    {
+        public readonly int Width;
+        public readonly int Height;
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ src/tower-defense/tower-defense/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tower_defense
+{
+    class Program
+    {
+        static void Main()
+        {
+            Map map = new Map(8, 5);
+
+            //map.Width = 8;
+            //map.Height = 5;
+
+            int area = map.Width * map.Height;
+            Console.WriteLine(area);
+        }
+    }
+}
